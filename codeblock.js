b6d96b7dff12e145c61ecc8d6d08a7ba8643ee15
Travis <travisnburt@gmail.com>
2021-05-13 18:01:14 -0600

    Add syntax highlighting injection

--- codeblock.js
+++ codeblock.js
@@ -1,8 +1,24 @@
-let codeblock = [...document.getElementsByTagName('codeblock')];
+const codeblock = [...document.getElementsByTagName('codeblock')];
 
-let keywords = ['let', 'var', 'function'];
+const keywords = ['const', 'let', 'var', 'function', 'class'];
+const controlwords = ['if', 'else', 'elseif', 'switch', 'case', 'for', 'do'];
 
 codeblock.map((code) => {
-  code.innerText = code.innerHTML+'\n';
+  let lines = code.innerHTML.split('\n');
+  let newCode = [];
+
+  const tabcount = lines[1].match(/([\s]+)/g).length;
+
+  for(let line of lines) {
+    for(let keyword of keywords) {
+      line = line.replace(`${keyword} `, `<span class='keyword'>${keyword}</span> `);
+    }
+    for(let controlword of controlwords) {
+      line = line.replace(`${controlword}`, `<span class='controlword'>${controlword}</span>`);
+    }
+    newCode.push(`${line}\n`);
+  }
+
+  code.innerHTML = newCode.join('');
 })
 
